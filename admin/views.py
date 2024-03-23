1a648046c2eb0fe2a6945c12cc4783bebdcb0dce
Nerusan <adamovnias91@gmail.com>
2024-03-23 10:33:28 +0200

    Fix Exesizes List View in admin

--- admin/views.py
+++ admin/views.py
@@ -269,7 +269,7 @@ class ExesizeView (TableView):
 class ExesizesView (TableView):
     def picture_validation(form, field):
         if field.data:
-            try:
+            #try:
                 filename = field.data.filename
                 if filename[-4:] != '.jpg' and filename[-4:] != '.png' and filename[-4:] != '.gif':
                     raise ValidationError('file must be .jpg or .png or gif')
@@ -282,8 +282,8 @@ class ExesizesView (TableView):
                 image.save(img_path)
                 field.data = url_for('static', filename=f"/images/{f_name}")
                 # setattr(form._obj, 'img_url', url_for('static', filename=f"/images/{f_name}"))
-            except:
-                f_name = field.data
+            #except:
+            #    f_name = field.data
 
     def on_model_change(view, context, model, name):
         setattr(model, 'img_url', context.img_url.data)
