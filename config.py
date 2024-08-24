711ba5123d01f0893a51cd0233258de51fe16d10
Nerusan <adamovnias91@gmail.com>
2024-08-24 23:35:50 +0300

    Fix Load env bug

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 # Create dummy secrey key so we can use sessions
 SECRET_KEY = os.getenv("SECRET_KEY")
 
