6862b81a1747a72f72fd417f33d487c8fc5496dd
Nias Adamov <126754149+NeysonAdams@users.noreply.github.com>
2024-11-28 18:44:10 +0200

    bug fix

--- admin/newadmin.py
+++ admin/newadmin.py
@@ -290,21 +290,17 @@ def level():
                         db.session.add(wordex)
 
                     for word in exec['word_ex']['words']:
-                        if word["id"] == -1:
-                            w = Wordslink()
-                        else:
-                            w = Wordslink.query.filter_by(id=word["id"]).first()
+                        w = Wordslink.query.filter_by(id=word["id"]).first()
 
                         if not w:
                             w = Wordslink()
+                            db.session.add(w)
+                            wordex.words.append(w)
 
                         w.eng = word["eng"]
                         w.rus = word["rus"]
                         w.uzb = word["uzb"]
 
-                        if word["id"] == -1:
-                            db.session.add(w)
-                            wordex.words.append(w)
 
 
                     exesize.wordexecesize = wordex
